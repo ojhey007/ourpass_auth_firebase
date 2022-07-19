38adec99666e7e82d9a01a4e65f65ea6023470f8
Oladayo <madfek@gmail.com>
2022-07-19 06:38:39 +0100

    login page view implementation

---
+++ lib/screens/login/view/login.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late String _email;
+  late String _password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Form(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      key: const Key('email'),
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Email can\'t be empty.' : null,
+                      onSaved: (val) => _email = val!,
+                    ),
+                    TextFormField(
+                      key: const Key('password'),
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      validator: (val) =>
+                          val!.isEmpty ? 'Password can\'t be empty.' : null,
+                      onSaved: (val) => _password = val!,
+                    ),
+                    const ElevatedButton(
+                        key: Key('login'),
+                        onPressed: null,
+                        child: Text('Login', style: TextStyle(fontSize: 20.0))),
+                  ],
+                )),
+              ],
+            )));
+  }
+}
